fccf8e2be60eaf9c37444b38297c15f22f635ebd
Raksha Mohta <rakshamohta123@gmail.com>
2021-06-19 18:38:45 +0530

    My First CommitFirstCommit

---
+++ OnlineNewsWebAppContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using OnlineNewsWebApp.Models;
+
+namespace OnlineNewsWebApp
+{
+    public class OnlineNewsWebAppContext: DbContext
+    {
+        public OnlineNewsWebAppContext() : base("OnlineNewsConnectionString")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public static OnlineNewsWebAppContext Create()
+        {
+            return new OnlineNewsWebAppContext();
+        }
+
+
+        public virtual DbSet<NewsCategory> NewsCategory { get; set; }
+        public virtual DbSet<Feedback> Feedbacks  { get; set; }
+        public virtual DbSet<International> Internationals{ get; set; }
+        public virtual DbSet<Subscribers> Subscribers { get; set; }
+        public virtual DbSet<Advertisement> Advertisements { get; set; }
+
+    }
+}
